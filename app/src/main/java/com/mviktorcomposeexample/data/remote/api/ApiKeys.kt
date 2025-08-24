fa7838c288c182b5d2ac33eb5469622000fca4e1
Anil Kumar <anilkumar@Anils-MacBook-Air.local>
2025-08-25 02:49:47 +0530

    updated key

--- app/src/main/java/com/mviktorcomposeexample/data/remote/api/ApiKeys.kt
+++ app/src/main/java/com/mviktorcomposeexample/data/remote/api/ApiKeys.kt
@@ -3,7 +3,7 @@ package com.mviktorcomposeexample.data.remote.api
 object ApiKeys {
     const val BASE_URL = "https://newsapi.org/v2/"
 
-    const val API_KEY = "8b07f123c81f4921a034c3aa4615c0f5"
+    const val API_KEY = "Your Api Key"
 
     const val TOP_HEADLINES = "top-headlines?"
 }
